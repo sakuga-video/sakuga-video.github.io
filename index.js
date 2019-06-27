c7baf0dfeb5f0f2e5bfb972a17a9b30e21eb552b
Cypress Frankenfeld <iamcypress@gmail.com>
2019-06-26 23:38:24 -0400

    Make it work with heroku

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ var express = require('express')
 var proxy = require('http-proxy-middleware')
  
 var app = express()
- 
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 app.use('/api', proxy({
     target: 'https://www.sakugabooru.com',
     changeOrigin: true,
     pathRewrite: {'^/api' : ''}
  }))
 app.use('/', express.static('public'))
-app.listen(3000)
+app.listen(port)
