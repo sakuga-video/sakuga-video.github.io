9ea200c3407da1856b1cc80f9497f8e687fad659
Cypress Frankenfeld <iamcypress@gmail.com>
2019-07-04 14:15:47 -0400

    Allow filtering by tag

--- public/script.js
+++ public/script.js
@@ -6,15 +6,66 @@ const nextButton = document.querySelector('#next');
 const controls = document.querySelector('#controls');
 const playPauseIcon = document.querySelector('#playpause i');
 const fullscreenIcon = document.querySelector('#fullscreen i');
+const input = document.querySelector('input');
+const tagsDatalist = document.querySelector('#tags');
 
-async function getRandomVideo() {
-    const url = '/api/post.json?limit=1&page=' + Math.floor(Math.random() * 15390) + '&tags=fighting';
+var tagText = [];
+var tagCounts = new Map();
+var playlist = [];
+var index = 0;
+var currentTag = null;
+
+function playTag(tag) {
+    currentTag = tag;
+    if (!tag) {
+        count = 80000;
+    } else {
+        count = tagCounts.get(tag);
+    }
+    playlist = shuffle(Array.from(Array(count).keys())
+        .map(n => ++n));
+    index = 0;
+    playNext();
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+async function getNextVideo() {
+    const nextId = playlist[index];
+    index = (index + 1) % playlist.length;
+
+    var url = '/api/post.json?limit=1&page=' + nextId;
+    if (currentTag) {
+        url = url + '&tags=' + currentTag.split(" ").join("_");
+    }
+
     const response = await fetch(url);
     const data = await response.json();
-    return data[0].file_url;
+    if (data && data[0] && data[0].file_url && data[0].file_ext === "mp4") {
+        return data[0].file_url;
+    } else {
+        return getNextVideo();
+    }
 }
 function playNext() {
-    getRandomVideo().then(src => {
+    getNextVideo().then(src => {
         video.src = src;
         video.play();
     });
@@ -72,16 +123,15 @@ playpauseButton.addEventListener('click', e => {
 });
 nextButton.addEventListener('click', playNext);
 
-getRandomVideo().then(src => {
-    video.src = src;
-    video.play();
-})
-
 var userActivity, activityCheck, inactivityTimeout;
 
 videoContainer.addEventListener('mousemove', function(){
     userActivity = true;
 });
+
+function textFocus() {
+    return document.activeElement === input;
+}
 
 activityCheck = setInterval(function() {
 
@@ -105,9 +155,41 @@ activityCheck = setInterval(function() {
       // Protect against the case where the inactivity timeout can trigger
       // before the next user activity is picked up  by the 
       // activityCheck loop.
-      if (!userActivity && !videoContainer.classList.contains('fade-out')) {
+      if (!userActivity
+        && !videoContainer.classList.contains('fade-out')
+        && !textFocus()) {
         videoContainer.classList.add('fade-out');
       }
     }, 1000);
   }
 }, 250);
+
+async function getTags() {
+    const response = await fetch('/api/tag.json?limit=0');
+    return await response.json();
+}
+function useTags(tags) {
+    var innerString = '';
+    for (tag of tags) {
+        innerString = innerString + "<option>" + tag + "</option>";
+    }
+    tagsDatalist.innerHTML = innerString;
+}
+input.addEventListener('input', () => {
+    if (!input.value || tagText.includes(input.value)) {
+        playTag(input.value);
+    }
+});
+input.addEventListener("keyup", event => {
+    if (event.key === "Enter") {
+        input.blur();
+    }
+});
+getTags().then(tags => {
+    const filteredTags = tags.filter(tag => tag.count > 50)
+    tagText = filteredTags.map(tag => tag.name.split("_").join(" "));
+    tagCounts = new Map(filteredTags.map(tag => [tag.name.split("_").join(" "), tag.count]));
+    useTags(tagText);
+});
+
+playTag();
